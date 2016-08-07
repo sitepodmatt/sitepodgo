159e52faac3405b4e39a42dfcb58a27ad7f7a8e8
Matt Freeman <matt@nowprovision.com>
2016-08-07 11:58:03 +0700

    fix setup scripts to current concepts

--- setup/create.sh
+++ setup/create.sh
@@ -2,9 +2,8 @@
 
 SERVER=http://127.0.0.1:8080
 
-~/binaries/kubernetes/kubectl -s=$SERVER create -f counter.yaml
 ~/binaries/kubernetes/kubectl -s=$SERVER create -f cluster.yaml
-~/binaries/kubernetes/kubectl -s=$SERVER create -f service.yaml
 ~/binaries/kubernetes/kubectl -s=$SERVER create -f sitepod.yaml
+~/binaries/kubernetes/kubectl -s=$SERVER create -f appcomponent.yaml
 ~/binaries/kubernetes/kubectl -s=$SERVER create -f systemuser.yaml
 
